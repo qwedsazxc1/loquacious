ca28073a10a4449dedaee72e8236dfb5c7c41cac
keep1earning <linyihong1234@gmail.com>
2023-12-06 23:29:00 +0800

    First commit Just only include a simple file to comunicate with chatgpt api and a simple README file and a empty gitignore.

---
+++ test.py
@@ -0,0 +1,12 @@
+from openai import OpenAI
+client = OpenAI()
+
+response = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Hi"},
+  ]
+)
+
+print(response)
